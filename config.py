ff1470cf4a9f7681ee91a1b37de72d3498137fc3
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2025-06-06 13:06:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = '7792344421:AAFD2mGg58Z51bhkqSFw5TOyd25HDVBHLfc'
 
 CHANNEL_USERNAME = '@twelvemua'
 
-DATABASE_URL = 'postgresql://postgres:iRCrXNhcwwiWBedEGcltcSvUKsFFcWjT@postgres-production-1a7d.up.railway.app:5432/railway?sslmode=require'
+DATABASE_URL = 'postgresql://postgres:iRCrXNhcwwiWBedEGcltcSvUKsFFcWjT@postgres.railway.internal:5432/railway'
 
 SHOP_ID = 'YOUR_SHOP_ID'
 SHOP_SECRET_KEY = 'YOUR_SECRET_KEY'
