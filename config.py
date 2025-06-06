fd252616dce6794b8fa8f948d4f6629680cc3775
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2025-06-06 11:57:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = 'YOUR_BOT_TOKEN_HERE'
+
+CHANNEL_USERNAME = '@twelvemua'
+
+DATABASE_URL = 'postgresql://postgres:iRCrXNhcwwiWBedEGcltcSvUKsFFcWjT@postgres.railway.internal:5432/railway'
+
+SHOP_ID = 'YOUR_SHOP_ID'
+SHOP_SECRET_KEY = 'YOUR_SECRET_KEY'
