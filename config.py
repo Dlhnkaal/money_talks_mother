76c6360bba714e0bf4457acb20339cf099315931
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2025-06-06 13:20:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 BOT_TOKEN = '7792344421:AAFD2mGg58Z51bhkqSFw5TOyd25HDVBHLfc'
 
 CHANNEL_USERNAME = '@twelvemua'
 
-DATABASE_URL = 'postgresql://postgres:iRCrXNhcwwiWBedEGcltcSvUKsFFcWjT@**postgres-production-1a7d.up.railway.app**:5432/railway'
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 SHOP_ID = 'YOUR_SHOP_ID'
 SHOP_SECRET_KEY = 'YOUR_SECRET_KEY'
