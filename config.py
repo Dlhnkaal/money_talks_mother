84b2cffc1d93b2c863efe109055348f6f64fbdc9
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2025-06-07 16:59:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-BOT_TOKEN = '7792344421:AAEcvmznP_NKvHtH0-roiC3fmj7IduhyPag'
-
+BOT_TOKEN = '7792344421:AAEcvmznP_NKvHth0-roic3fmj7IduhyPag'
 CHANNEL_USERNAME = '@twelvemua'
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 SHOP_ID = '1102570'
-SHOP_SECRET_KEY = 'test_pDQVvRW0fLkmBQfDc5H_ALXUywWW8V3OBQS_ed_2xKM'
+SHOP_SECRET_KEY = 'test_pOQVVRWofLkmBQfDc5H_ALXUywMM8V30BQS_ed_2xKN'
+
+# Новые параметры для Render
+RENDER_DOMAIN = "money-talks-mother.onrender.com"
+PORT = int(os.getenv("PORT", "10000"))  # Render использует порт 10000
