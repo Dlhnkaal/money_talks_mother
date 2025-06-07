c3a006d1d25c6265a1ccdc49c95d33353c861171
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2025-06-07 17:22:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
-BOT_TOKEN = '7792344421:AAEcvmznP_NKvHth0-roic3fmj7IduhyPag'
+# Важно: используйте os.getenv() для безопасности
+BOT_TOKEN = os.environ['BOT_TOKEN']  # Обязательно через переменные окружения
 CHANNEL_USERNAME = '@twelvemua'
 DATABASE_URL = os.getenv("DATABASE_URL")
 SHOP_ID = '1102570'
 SHOP_SECRET_KEY = 'test_pOQVVRWofLkmBQfDc5H_ALXUywMM8V30BQS_ed_2xKN'
 
-# Новые параметры для Render
-RENDER_DOMAIN = "money-talks-mother.onrender.com"
-PORT = int(os.getenv("PORT", "10000"))  # Render использует порт 10000
+# Настройки для Render
+RENDER_EXTERNAL_URL = os.environ['RENDER_EXTERNAL_URL']  # Автоматически задаётся Render
+PORT = int(os.environ.get("PORT", "10000"))
