537440529f5b0101623b4a5fd7e349617dfdfa50
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2025-06-07 18:58:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ CHANNEL_USERNAME = '@twelvemua'
 DATABASE_URL = os.getenv("DATABASE_URL")
 SHOP_ID = '1102570'
 SHOP_SECRET_KEY = 'test_pOQVVRWofLkmBQfDc5H_ALXUywMM8V30BQS_ed_2xKN'
+PAY_URL = ""
 
 # Настройки для Render
 RENDER_EXTERNAL_URL = os.environ['RENDER_EXTERNAL_URL']  # Автоматически задаётся Render
